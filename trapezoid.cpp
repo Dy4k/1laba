cf8b25fe4c79cf254c10c1859a996a9bf0e0467f
danil <danildja4enko@gmail.com>
2025-03-17 11:24:34 +0700

    изменения внесены

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -15,6 +16,21 @@ int main() {
     cout << "Введите длину второй боковой стороны: ";
     cin >> side2;
 
+    // Проверка на неотрицательные значения
+    if (base1 <= 0 || base2 <= 0 || height <= 0 || side1 <= 0 || side2 <= 0 || height > side1 || height > side2) {
+        cout << "Ошибка." << endl;
+        return 1;
+    }
+
+    // Проверка: сумма любых трёх сторон должна быть больше четвёртой
+    if ((base1 + base2 + side1 <= side2) ||
+        (base1 + base2 + side2 <= side1) ||
+        (base1 + side1 + side2 <= base2) ||
+        (base2 + side1 + side2 <= base1)) {
+        cout << "Ошибка: сумма любых трёх сторон должна быть больше длины четвёртой стороны." << endl;
+        return 1;
+    }
+
     // Средняя линия
     double midline = (base1 + base2) / 2;
 
@@ -28,6 +44,3 @@ int main() {
     cout << "Длина средней линии: " << midline << endl;
     cout << "Периметр трапеции: " << perimeter << endl;
     cout << "Площадь трапеции: " << area << endl;
-
-    return 0;
-}
